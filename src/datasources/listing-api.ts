1579793f80f1888f488da1357d18815bbfe1f619
Vitalii.B <vitalii.bondarenko.ua@gmail.com>
2025-07-26 18:29:34 +0300

    add resolver chains

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,5 +1,5 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { Listing } from "../types";
+import { Listing, Amenity } from "../types";
 
 export class ListingAPI extends RESTDataSource {
 
@@ -7,5 +7,13 @@ export class ListingAPI extends RESTDataSource {
 
     getFeaturedListings(): Promise<Listing[]> {
         return this.get<Listing[]>("featured-listings");
+    }
+
+    getListing(listingId: string): Promise<Listing> {
+        return this.get<Listing>(`listings/${listingId}`);
+    }
+
+    getAmenities(listingId: string): Promise<Amenity[]> {
+        return this.get<Amenity[]>(`listings/${listingId}/amenities`);
     }
 }
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,9 +1,20 @@
-import { DataSourceContext } from "./context";
+import { Resolvers } from "./types";
+import { validateFullAmenities } from "./helpers";
 
-export const resolvers = {
+export const resolvers: Resolvers = {
     Query: {
-        featuredListings: (_: any, __: any, {  dataSources }: DataSourceContext) => {
+        featuredListings: (_, __, {  dataSources }) => {
             return dataSources.listingAPI.getFeaturedListings();
         },
+        listing: (_, { id }, { dataSources }) => {
+            return dataSources.listingAPI.getListing(id);
+        },
+    },
+    Listing: {
+        amenities: ({ id, amenities }, _, { dataSources }) => {
+            return validateFullAmenities(amenities)
+                ? amenities
+                : dataSources.listingAPI.getAmenities(id);
+        }
     },
 };
